c6a4dc62d5ff763b9c2f36340b14e28426e2a805
guru <u1010032@utah.edu>
2016-10-24 04:36:52 -0600

    fixed the code to read input from file

--- calcc.cpp
+++ calcc.cpp
@@ -25,8 +25,6 @@ int  gLineNo = 1;
 bool debug = false;
 int  gOffset = 0;
 
-
-
 Value* parseExpression(string tab);
 void skipSpaces(string tab);
 
@@ -437,6 +435,7 @@ Value* parser(string tab) {
     printf("%sExit %s\r\n", tab.c_str(), __PRETTY_FUNCTION__);
     printf("Parsed successfully\r\n");
   }
+  printf("Parsed successfully\r\n");
   return result;
 }
 
